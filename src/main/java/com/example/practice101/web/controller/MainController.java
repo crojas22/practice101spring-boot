8a00d6e523baf9b3ca83ba5f6e1afe119d5d61b1
Cesar R <crojas22@comcast.net>
2017-12-06 12:31:08 -0500

    Initial commit

---
+++ src/main/java/com/example/practice101/web/controller/MainController.java
@@ -0,0 +1,23 @@
+package com.example.practice101.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+
+    @RequestMapping("/")
+    public String getMainPage() {
+        return "index";
+    }
+
+    @RequestMapping("/registration")
+    public String getRegistrationPage() {
+        return "registration";
+    }
+
+    @RequestMapping("/login")
+    public String getLoginPage() {
+        return "login";
+    }
+}
